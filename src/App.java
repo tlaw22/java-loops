1a2ae18082e3e5235fcd13255e1b49555ba9d906
tlaw22 <timothy.j.lawless@youtlook.com>
2024-01-01 17:03:32 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        String name = "A whole bunch of random letters";
+        System.out.println(name.length());
+        int counter = 0;
+        for (int idx = 0; idx < 100; idx++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("the value of idx was " + idx + "===========" + j);
+            }
+
+        }
+    }
+}
